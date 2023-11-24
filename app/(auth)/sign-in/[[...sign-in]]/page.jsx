cde5f4b559973c246d3ea083e05ef75275b3ca20
Rahul Sanap <rahulsanap@Rahuls-MacBook-Air.local>
2023-11-24 09:04:07 -0500

    Updated SignIn UI

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -7,22 +7,22 @@ export default function Page() {
   
  
 
-<section class="bg-white">
-  <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
+<section className="bg-white">
+  <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
     <section
-      class="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6"
+      className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6"
     >
       <img
         alt="Night"
         src="https://images.unsplash.com/photo-1617195737496-bc30194e3a19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-        class="absolute inset-0 h-full w-full object-cover opacity-80"
+        className="absolute inset-0 h-full w-full object-cover opacity-80"
       />
 
-      <div class="hidden lg:relative lg:block lg:p-12">
-        <a class="block text-white" href="/">
-          <span class="sr-only">Home</span>
+      <div className="hidden lg:relative lg:block lg:p-12">
+        <a className="block text-white" href="/">
+          <span className="sr-only">Home</span>
           <svg
-            class="h-8 sm:h-10"
+            className="h-8 sm:h-10"
             viewBox="0 0 28 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -34,28 +34,28 @@ export default function Page() {
           </svg>
         </a>
 
-        <h2 class="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+        <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
           Welcome to Tubeguruji LMS 🦑
         </h2>
 
-        <p class="mt-4 leading-relaxed text-white/90">
+        <p className="mt-4 leading-relaxed text-white/90">
          Now You can Learn , Track your progress inside this application.
         </p>
       </div>
     </section>
 
     <main
-      class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+      className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
     >
-      <div class="max-w-xl lg:max-w-3xl">
-        <div class="relative -mt-16 block lg:hidden">
+      <div className="max-w-xl lg:max-w-3xl">
+        <div className="relative -mt-16 block lg:hidden">
           <a
-            class="inline-flex h-16 w-16 items-center justify-center rounded-full bg-white text-blue-600 sm:h-20 sm:w-20"
+            className="inline-flex h-16 w-16 items-center justify-center rounded-full bg-white text-blue-600 sm:h-20 sm:w-20"
             href="/"
           >
-            <span class="sr-only">Home</span>
+            <span className="sr-only">Home</span>
             <svg
-              class="h-8 sm:h-10"
+              className="h-8 sm:h-10"
               viewBox="0 0 28 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -68,12 +68,12 @@ export default function Page() {
           </a>
 
           <h1
-            class="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl"
+            className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl"
           >
             Welcome to Squid 🦑
           </h1>
 
-          <p class="mt-4 leading-relaxed text-gray-500">
+          <p className="mt-4 leading-relaxed text-gray-500">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi
             nam dolorum aliquam, quibusdam aperiam voluptatum.
           </p>
